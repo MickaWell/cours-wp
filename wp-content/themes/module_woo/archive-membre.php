5ba559ebb6ff256d1f3d239a9ce88dd08462eba1
cheick92i <cheick92i@gmail.com>
2014-06-04 10:39:47 +0200

    membres

--- wp-content/themes/module_woo/archive-membre.php
+++ wp-content/themes/module_woo/archive-membre.php
@@ -1,26 +1,21 @@
 <?php
 /* 
-Template Name: Archives
+Template Name: membres
 */
 ?>
 <?php get_header(); ?>
 <div class="col-md-12 bloc">
+    <?php //ouverture d'une nouvelle boucle qui va chercher nos livres
+      $books_loop = new WP_Query( 'post_type=membre');
+        if( $books_loop->have_posts() ):
+           while( $books_loop->have_posts() ): $books_loop->the_post();?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+              <h1> <?php the_field("date_de_naissance"); ?> </h1>
+            </article>
+           <?php endwhile;
+        endif;
+      wp_reset_postdata();
+      ?>
     
-     <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-      <div class="post">
-          <?php the_post_thumbnail(); ?>
-      <h1 class="post-title"><?php the_title(); ?></h1>
-        
-
-        
-      
-        <div class="post-content">
-          <?php the_content(); ?>
-        </div>
-        
-      </div>
-    <?php endwhile; ?>
-  <?php endif; ?>
 </div>
 <?php get_footer(); ?>
