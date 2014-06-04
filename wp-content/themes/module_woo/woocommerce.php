4bd46f1616ed2f93c024952883a204715bf570b8
cheick92i <cheick92i@gmail.com>
2014-06-04 10:49:00 +0200

    marche ap

--- wp-content/themes/module_woo/woocommerce.php
+++ wp-content/themes/module_woo/woocommerce.php
@@ -3,21 +3,15 @@
 <div class="col-md-12 bloc">
     
      <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-      <div class="post">
-          <?php the_post_thumbnail(); ?>
-      <h1 class="post-title"><?php the_title(); ?></h1>
-        <h1 class="post-info" >
-      <?php the_date('j M Y ') ?>
-        </h1>
-        
+  
+  
       
         <div class="post-content">
           <?php woocommerce_content(); ?>
         </div>
         
       </div>
-    <?php endwhile; ?>
+  
   <?php endif; ?>
 </div>
 <?php get_footer(); ?>
